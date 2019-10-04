b08e1a80cff03b87aebbeaeca5ed7d1cd97a29c7
Larry Shields <shieldsl@upmc.edu>
2019-10-04 12:06:24 -0400

    App Creation

---
+++ src/test/java/com/lswebworld/hl7fixer/Hl7FixerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lswebworld.hl7fixer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Hl7FixerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
